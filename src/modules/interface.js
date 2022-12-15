eb35923e965b3458d8d5b890ef70e64167ed7cb9
Samson Cheung <samsonc89@yahoo.com>
2022-12-15 13:37:12 -0800

    Remove display of project

--- src/modules/interface.js
+++ src/modules/interface.js
@@ -1,17 +1,28 @@
 import { taskList, projectsList } from "../index.js";
 
 const tasksDisplay = document.querySelector(".tasks-display");
+const projectsDisplay = document.querySelector(".projects-display");
 
 function updateTasksUI(task) {
   const html = `<div class="card" >
     <h3 class="book-title">${task.title}</h3>
     <p class="">Due: ${task.dueDate}</p>
     <p class="">Notes: ${task.notes}</p>
-    <p class="">Project: ${task.project}</p>
 
     </div>`;
 
   tasksDisplay.insertAdjacentHTML("beforeend", html);
 }
+function updateProjectsUI(task) {
+  const html = `<div class="card" >
+    <h3 class="book-title">${task.title}</h3>
+    <p class="">Due: ${task.dueDate}</p>
+    <p class="">Notes: ${task.notes}</p>
 
-export { updateTasksUI };
+
+    </div>`;
+
+  projectsDisplay.insertAdjacentHTML("beforeend", html);
+}
+
+export { updateTasksUI, updateProjectsUI };
