7b2d75580b613e667800e86d8df155e100ae679c
Samson Cheung <samsonc89@yahoo.com>
2022-12-16 10:51:35 -0800

    Add function to make text strikethrough when checkbox is checked

--- src/modules/interface.js
+++ src/modules/interface.js
@@ -3,24 +3,26 @@ const projectsDisplay = document.querySelector(".projects-display");
 
 function updateTasksUI(task) {
   const html = `<div class="card task-card" >
-    <h3 class="task-title">${task.title}</h3>
-    <p class="">Due: ${task.dueDate}</p>
-    <p class="">Notes: ${task.notes}</p>
-
+  
+    <h3 class="task-title"><input type="checkbox" onchange="checkTask(this)">${task.title}</h3>
     </div>`;
 
   tasksDisplay.insertAdjacentHTML("beforeend", html);
 }
-function updateProjectsUI(task) {
+function updateProjectsUI(project) {
   const html = `<div class="card project-card" >
-    <h3 class="project-title">${task.title}</h3>
-    <p class="">Due: ${task.dueDate}</p>
-    <p class="">Notes: ${task.notes}</p>
-
-
+    <h3 class="project-title">${project.title}</h3>
     </div>`;
 
   projectsDisplay.insertAdjacentHTML("beforeend", html);
 }
 
-export { updateTasksUI, updateProjectsUI };
+function checkTask(checkboxElem) {
+  if (checkboxElem.checked) {
+    console.log(event.target.parentElement.classList.add("done"));
+  } else {
+    console.log(event.target.parentElement.classList.remove("done"));
+  }
+}
+
+export { updateTasksUI, updateProjectsUI, checkTask };
